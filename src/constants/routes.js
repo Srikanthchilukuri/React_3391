e1c528a174d32090c0b71073a1e5e209041e179c
unknown <srikanthchilukuri01@gmail.com>
2021-06-12 21:36:38 +0530

    src

---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+export const SIGN_UP = '/Signup';
+export const SIGN_IN = '/Signin';
+export const LANDING = '/';
+export const HOME = '/Home';
+export const ACCOUNT = '/Account';
+export const ADMIN = '/Admin';
+export const PASSWORD_FORGET = '/PasswordForget';
